71e43b4b7a6d4023abf346f38854e408fb40b269
sepehrg <goodarzyar@gmail.com>
2026-08-14 18:55:08 +1000

    add search functionality

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 
 from .models import Donut
 from .serializers import DonutSerializer
@@ -7,3 +8,14 @@ class DonutViewSet(ModelViewSet):
   queryset = Donut.objects.all()
   serializer_class = DonutSerializer
 
+  def get_queryset(self):
+    queryset = super().get_queryset()
+
+    query = self.request.query_params.get("query")
+
+    if query:
+      queryset = queryset.filter(Q(name__icontains = query) | Q(description__icontains = query))
+
+    return queryset
+
+    
