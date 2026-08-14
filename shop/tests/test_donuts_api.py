bdfc444ddd5e92f3cc5e216bf91167a31a847cce
sepehrg <goodarzyar@gmail.com>
2026-08-14 13:50:14 +1000

    add test individual donut

--- shop/tests/test_donuts_api.py
+++ shop/tests/test_donuts_api.py
@@ -7,7 +7,7 @@ from rest_framework.test import APITestCase
 from shop.models import Donut
 
 
-class DonutListTests(APITestCase):
+class DonutTests(APITestCase):
 
     def test_list_donuts(self):
         Donut.objects.create(
@@ -23,3 +23,29 @@ class DonutListTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["code"], "CHOCOLATE-01")
+
+
+    def test_get_donut(self):
+        donut = Donut.objects.create(
+            code="CHOCOLATE-01",
+            name="Chocolate donut",
+            description="Chocolate donut description",
+            price=Decimal("3.50"),
+            is_available=True,
+        )
+
+        response = self.client.get(reverse("donut-detail", args=[donut.id]))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+              {
+                  "id": donut.id,
+                  "name": "Chocolate donut",
+                  "code": "CHOCOLATE-01",
+                  "description":"Chocolate donut description",
+                  "price": "3.50",
+                  "is_available": True,
+              }
+          )
+
