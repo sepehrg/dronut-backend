be33e4e454863c8155ea62cd8807261e1fcb3476
sepehrg <goodarzyar@gmail.com>
2026-08-14 12:31:08 +1000

    unify env variables

--- dronut/settings.py
+++ dronut/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'dronut.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["DB_NAME"],
-        'USER': os.environ["DB_USER"],
-        'PASSWORD': os.environ["DB_PASSWORD"],
-        'HOST': os.environ.get("DB_HOST", "localhost"),
-        'PORT': os.environ.get("DB_PORT", "5432")
+        'NAME': os.environ["POSTGRES_DB"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+        'HOST': os.environ.get("POSTGRES_HOST", "localhost"),
+        'PORT': os.environ.get("POSTGRES_PORT", "5432")
     }
 }
 
