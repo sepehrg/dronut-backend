c23f5ba16724ec69c8d31e36a064bb846e1088c9
sepehrg <goodarzyar@gmail.com>
2026-08-14 12:46:02 +1000

    add donut serializer

---
+++ shop/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Donut
+
+class DonutSerializer(serializers.ModelSerializer):
+  class Meta: model = Donut
+  fields = [
+    "id",
+    "name",
+    "code",
+    "description",
+    "price",
+    "is_available",
+  ]
